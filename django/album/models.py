83d03e3ddfca5060f14dd14a5e040cf7f46524a5
Ryu Taehwan <yth3260@gmail.com>
2018-02-08 11:18:54 +0900

    Album model의 str 메서드 작성

--- django/album/models.py
+++ django/album/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+from artist.models import Artist
+
+
+class Album(models.Model):
+    artist = models.ManyToManyField(Artist, related_name='album_list')
+    album_name = models.CharField('앨범명', max_length=50)
+    release_date = models.DateField('발매일', null=False, blank=False)
+    genre = models.CharField('장르', max_length=50, blank=True)
+    album_intro = models.TextField('소개', blank=True)
+    img_cover = models.ImageField(
+        '커버 이미지',
+        upload_to='album',
+        blank=True,
+    )
+
+    # @property
+    # def genre(self):
+    #     return ''
+    #
+    def __str__(self):
+        artist = ', '.join(self.artist.values_list('name', flat=True))
+        return '{title} [{artist}]'.format(
+            title = self.album_name,
+            artist = artist,
+        )
--- django/song/models.py
+++ django/song/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+from album.models import Album
+from artist.models import Artist
+
+
+class Song(models.Model):
+    artist = models.ManyToManyField(Artist, through='ArtistSong', related_name='song_list')
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    title = models.CharField(max_length=50)
+    lyrics = models.TextField(blank=True)
+    composer = models.CharField('작곡', max_length=50)
+    lyricist = models.CharField('작사', max_length=50)
+    arranger = models.CharField('편곡', max_length=50)
+
+    def __str__(self):
+        return self.title
+
+
+class ArtistSong(models.Model):
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    demo_date = models.DateField(null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.artist} | {self.song.title}'
