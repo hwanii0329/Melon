ae8df2e1b7f1623081f5daab8c9aa6c394531ed6
Ryu Taehwan <yth3260@gmail.com>
2018-02-08 12:04:25 +0900

    Song model 작성

--- django/song/models.py
+++ django/song/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 from album.models import Album
 from artist.models import Artist
+import datetime
 
 
 class Song(models.Model):
@@ -9,12 +10,29 @@ class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     title = models.CharField(max_length=50)
     lyrics = models.TextField(blank=True)
-    composer = models.CharField('작곡', max_length=50)
-    lyricist = models.CharField('작사', max_length=50)
-    arranger = models.CharField('편곡', max_length=50)
+    genre = models.CharField(max_length=100, blank=True)
+    composer = models.CharField('작곡', max_length=50, blank=True)
+    lyricist = models.CharField('작사', max_length=50, blank=True)
+    arranger = models.CharField('편곡', max_length=50, blank=True)
+
+    @property
+    def artist(self):
+        return self.album.artist.all()
+
+    @property
+    def release_date(self):
+        return self.album.release_date
+
+    @property
+    def formatted_release_date(self):
+        return self.release_date.strftime('%Y.%m.%d')
 
     def __str__(self):
-        return self.title
+        return '{artist} - {title} ({album})'.format(
+            artist=', '.join(self.album.artist.values_list('name', flat=True)),
+            title=self.title,
+            album=self.album.album_name,
+        )
 
 
 class ArtistSong(models.Model):
